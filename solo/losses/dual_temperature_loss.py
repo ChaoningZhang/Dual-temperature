81320daa38c56b72dc4d08080e0e8129f0f479a7
Zak <34052749+pantheon5100@users.noreply.github.com>
2022-12-01 14:22:04 +0900

    update comment

--- solo/losses/dual_temperature_loss.py
+++ solo/losses/dual_temperature_loss.py
@@ -27,10 +27,11 @@ def dual_temperature_loss_func(
     dt_m=10,
 ) -> torch.Tensor:
     """
-    query: anchor sample
-    key: positive sample
-    temperature: intra-anchor hardness-awareness control temperature
-    dt_m: the scalar number to get inter-anchor hardness awareness temperature
+    query: anchor sample.
+    key: positive sample.
+    temperature: intra-anchor hardness-awareness control temperature.
+    dt_m: the scalar number to get inter-anchor hardness awareness temperature.
+          inter-anchor hardness awareness temperature is calculated by dt_m * temperature
     """
 
     # intra-anchor hardness-awareness
